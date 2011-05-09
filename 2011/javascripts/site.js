bf0400dfdc6b2c5093559b21c95d49a6f306ae01
Habibullah Pagarkar <habib@thoughtworks.com>
2011-05-09 23:28:29 +0530

    Chandan Jog and Habibullah Pagarkar - Spiking out a twitter-ish view for talks in order to better utilize space. WIP.

---
+++ 2011/javascripts/site.js
@@ -0,0 +1,91 @@
+;
+(function($) {
+  $(document).ready(function() {
+
+    var selectedContributor = undefined;
+    var contributorDetailsSlider = $('#contributorDetailsSlider .content');  
+    var contributorDetailsClose = $('#contributorDetailsSlider .holder .close');
+
+    $('li.contributor').each(function(index,contributor){
+
+        $(contributor).bind('click',function(){
+
+            var self = $(this);
+
+            if(selectedContributor === undefined){
+                self.addClass('selected');
+                self.find('.toggleImage').addClass('leftArrow');
+                contributorDetailsSlider.append(self.find('.details').clone().show()).animate({ width: 'toggle'});
+                contributorDetailsSlider.addClass('open');
+                contributorDetailsClose.show('slow');
+            }
+
+            if(selectedContributor  !== undefined && selectedContributor[0] != self[0]){
+                selectedContributor.removeClass('selected');
+                selectedContributor.find('.toggleImage').removeClass('leftArrow');
+
+                self.addClass('selected');
+                self.find('.toggleImage').addClass('leftArrow');
+
+                contributorDetailsSlider.empty();
+                if(contributorDetailsSlider.hasClass('open')){
+                    contributorDetailsSlider.append(self.find('.details').clone().show());
+                }
+                else{
+                    contributorDetailsSlider.append(self.find('.details').clone().show()).animate({width: 'toggle'});
+                    contributorDetailsSlider.addClass('open')
+                    contributorDetailsClose.show('slow');
+                }
+            }
+
+            if(selectedContributor  !== undefined && selectedContributor[0] === self[0]){
+
+                contributorDetailsSlider.animate({width: 'toggle'});
+                if(contributorDetailsSlider.hasClass('open')){
+                    contributorDetailsSlider.removeClass('open');
+                    contributorDetailsClose.hide();
+                    self.find('.toggleImage').removeClass('leftArrow');
+                    self.removeClass('selected');
+                }
+                else{
+                    contributorDetailsSlider.addClass('open');
+                    contributorDetailsClose.show('slow');
+                    self.find('.toggleImage').addClass('leftArrow');
+                    self.addClass('selected');
+                }
+            }
+
+            selectedContributor = self;
+
+        });
+        
+
+        $(contributor).bind('mouseover',function(){
+            var self = $(this);
+            self.find('.toggleImage').addClass('rightArrow');
+            self.addClass('highlighted');
+
+        });
+
+        $(contributor).bind('mouseout',function(){
+            var self = $(this);
+            self.find('.toggleImage').removeClass('rightArrow');
+            self.removeClass('highlighted');
+        });
+
+    });
+
+
+    contributorDetailsClose.click(function(){
+        contributorDetailsSlider.animate({width: 'toggle'});
+        selectedContributor.removeClass('highlighted');
+        selectedContributor.find('.toggleImage').removeClass('leftArrow');
+        selectedContributor.removeClass('selected');
+        contributorDetailsSlider.removeClass('open');
+        contributorDetailsClose.hide();
+        
+    });
+
+  });
+})(jQuery);
+
